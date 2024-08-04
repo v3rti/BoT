c208b0a5973385b7fa9de9e50ed9dd34b2b8b30b
Amine Bagui <med.bagui.1@gmail.com>
2024-08-04 18:50:31 +0100

    p2 update, removed unnecessary js code

--- p2/script.js
+++ p2/script.js
@@ -1,14 +1,8 @@
-const currentTime = new Date();
-const hands = document.querySelectorAll('.hand');
-
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
 let hourDeg = 0;
-let minuteDeg = 0;
-let secondDeg = 0;
-
 
 setInterval(() => {
   let hours = new Date().getHours();
@@ -20,11 +14,7 @@ setInterval(() => {
     hourDeg = 30 * hours;
   }
 
-  minuteDeg = 360 / 60 * minutes;
-  secondDeg = 360 / 60 * seconds;
-
   hourHand.style.transform = `rotate(${hourDeg}deg)`
-  minuteHand.style.transform = `rotate(${minuteDeg}deg)`
-  secondHand.style.transform = `rotate(${secondDeg}deg)`
-
+  minuteHand.style.transform = `rotate(${(360 / 60) * minutes}deg)`
+  secondHand.style.transform = `rotate(${(360 / 60) * seconds}deg)`
 }, 500);
